37f54f15bf799f1a407aed8a336b754c1ac8a607
whynotandwhy <whynotandwhy@gmail.com>
2017-07-06 21:21:25 -0400

    Default project
    A first attempt testing things out

---
+++ Mechlick/MechlickPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Mechlick
+{
+    public partial class MechlickPage : ContentPage
+    {
+        public MechlickPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
